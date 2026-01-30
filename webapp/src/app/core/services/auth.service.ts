b61f9574ead893dd5b2c87e711705a933fe26cb1
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 04:28:46 +0100

    feat: add auth service

---
+++ webapp/src/app/core/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, switchMap, tap } from 'rxjs';
+import { UserService } from './user.service';
+import { User } from '../../shared/models/user';
+import { BASE_URL } from '../../shared/models/base-url';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private http = inject(HttpClient);
+  private baseUrl = inject(BASE_URL);
+
+  private userService = inject(UserService);
+
+  signup(email: string, password: string): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/auth/signup`, { email, password });
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http
+      .post(`${this.baseUrl}/auth/login`, { email, password }, { withCredentials: true })
+      .pipe(switchMap(() => this.userService.getUser()));
+  }
+
+  logout(): Observable<void> {
+    return this.http
+      .post<void>(`${this.baseUrl}/auth/logout`, {}, { withCredentials: true })
+      .pipe(tap(() => window.location.reload()));
+  }
+}
