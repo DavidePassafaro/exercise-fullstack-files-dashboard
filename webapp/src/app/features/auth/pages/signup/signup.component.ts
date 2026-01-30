8acc0c88bb028dc580243c81c847238cb1ee573f
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 06:00:33 +0100

    feat: added signup page

---
+++ webapp/src/app/features/auth/pages/signup/signup.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+
+@Component({
+  selector: 'csv-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+  imports: [RouterLink, ReactiveFormsModule],
+})
+export class SignupComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.authService.signup(this.form.value.email!, this.form.value.password!).subscribe(() => {
+        this.router.navigate(['/dashboard']);
+      });
+    }
+  }
+}
