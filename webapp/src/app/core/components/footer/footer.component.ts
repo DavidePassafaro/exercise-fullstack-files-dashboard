e7248dea2b3635bc6677867864a58a9a621b2f23
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 04:39:02 +0100

    feat: create empty footer component

---
+++ webapp/src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'csv-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
