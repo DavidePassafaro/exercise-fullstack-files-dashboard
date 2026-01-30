c84f1240e361fd8668325d476133641a5ae67b1e
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 06:00:44 +0100

    feat: added login page

---
+++ webapp/src/app/features/auth/pages/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'csv-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  imports: [RouterLink],
+})
+export class LoginComponent {}
