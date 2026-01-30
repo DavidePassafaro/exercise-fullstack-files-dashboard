4d0eb67e2e89cb8aafb9139e61f0c8ca0c6b45ca
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 06:04:48 +0100

    feat: add dashboard page

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -10,10 +10,10 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./features/auth/routes').then((m) => m.routes),
   },
-  //   {
-  //     path: 'dashboard',
-  //     loadChildren: () => import('./features/dashboard/routes').then((m) => m.routes),
-  //   },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./features/dashboard/routes').then((m) => m.routes),
+  },
   {
     path: '**',
     redirectTo: 'auth/login',
---
+++ webapp/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'csv-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent {}
