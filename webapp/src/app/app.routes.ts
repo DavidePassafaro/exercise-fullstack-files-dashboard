9256197bf214a7a7028e323f19da8792ca26106a
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 06:01:01 +0100

    feat: added routes

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/routes').then((m) => m.routes),
+  },
+  //   {
+  //     path: 'dashboard',
+  //     loadChildren: () => import('./features/dashboard/routes').then((m) => m.routes),
+  //   },
+  {
+    path: '**',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+];
