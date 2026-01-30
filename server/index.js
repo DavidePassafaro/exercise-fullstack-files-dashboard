f11bb39401582f9586d3cd75323c59f0b170e502
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 03:17:12 +0100

    feat: add user routes

--- server/index.js
+++ server/index.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 
 // Use authentication routes
 require("./routes/authRoutes")(app);
+require("./routes/userRoutes")(app);
 
 // Start the server
 app.listen(PORT, () => {
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+module.exports = (app) => {
+  app.get("/user/me", (req, res) => {
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    const user = User.findById(token);
+    if (!user) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    res.status(200).send(user);
+  });
+};
