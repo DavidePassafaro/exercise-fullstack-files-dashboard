b323d9ac5b637b2dbf24ab7e60550397b2ec3f4c
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 04:28:35 +0100

    feat: add user service

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
+import { UserService } from './core/services/user.service';
+import { BASE_URL } from './shared/models/base-url';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAppInitializer(() => {
+      const userService = inject(UserService);
+
+      return userService.getUser().pipe(catchError(() => of(null)));
+    }),
+
+    { provide: BASE_URL, useValue: 'http://localhost:3000' },
+  ],
 };
---
+++ webapp/src/app/core/services/user.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../shared/models/user';
+import { Observable, tap } from 'rxjs';
+import { BASE_URL } from '../../shared/models/base-url';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  private http = inject(HttpClient);
+  private baseUrl = inject(BASE_URL);
+
+  private _user = signal<User | null>(null);
+
+  public user = this._user.asReadonly();
+
+  getUser(): Observable<User> {
+    return this.http
+      .get<User>(`${this.baseUrl}/user/me`, { withCredentials: true })
+      .pipe(tap((user: User) => this._user.set(user)));
+  }
+}
