3c10915de22127525428b9e8541ffb0d6b3eed39
Davide Passafaro <passafaro@hotmail.it>
2026-01-30 06:17:09 +0100

    feat: add temporary logout in header

--- webapp/src/app/core/components/header/header.component.ts
+++ webapp/src/app/core/components/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'csv-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private authService = inject(AuthService);
+
+  onUserClick(): void {
+    this.authService.logout().subscribe();
+  }
+}
