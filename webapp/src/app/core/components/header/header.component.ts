53c65d17de3f68237d715bcc332572dc28e8102a
Davide Passafaro <passafaro@hotmail.it>
2026-02-01 02:51:10 +0100

    feat: improve header styles and breadcrumb

--- webapp/src/app/core/components/header/header.component.ts
+++ webapp/src/app/core/components/header/header.component.ts
@@ -1,16 +1,63 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
+
+interface Breadcrumb {
+  label: string;
+  url: string;
+}
 
 @Component({
   selector: 'csv-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink],
 })
 export class HeaderComponent {
   private authService = inject(AuthService);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+
+  protected breadcrumbs = signal<Breadcrumb[]>([]);
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      const breadcrumbs = this.createBreadcrumbs(this.activatedRoute.root);
+      this.breadcrumbs.set(breadcrumbs);
+    });
+  }
 
   onUserClick(): void {
     this.authService.logout().subscribe();
+  }
+
+  private createBreadcrumbs(
+    route: ActivatedRoute,
+    url: string = '',
+    breadcrumbs: Breadcrumb[] = [],
+  ): Breadcrumb[] {
+    const children: ActivatedRoute[] = route.children;
+
+    if (children.length === 0) {
+      return breadcrumbs;
+    }
+
+    for (const child of children) {
+      const routeURL: string = child.snapshot.url.map((segment) => segment.path).join('/');
+      if (routeURL !== '') {
+        url += `/${routeURL}`;
+      }
+
+      const label = child.snapshot.title;
+      if (label) {
+        breadcrumbs.push({ label, url });
+      }
+
+      return this.createBreadcrumbs(child, url, breadcrumbs);
+    }
+
+    return breadcrumbs;
   }
 }
