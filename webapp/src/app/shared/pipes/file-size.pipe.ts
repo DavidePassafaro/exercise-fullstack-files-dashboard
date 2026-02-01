f53d760a1b49f720f2f31657150f05c789464e37
Davide Passafaro <passafaro@hotmail.it>
2026-02-01 02:21:52 +0100

    feat: add file-size pipe

---
+++ webapp/src/app/shared/pipes/file-size.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'fileSize' })
+export class FileSizePipe implements PipeTransform {
+  transform(value: number): string {
+    if (value === 0) return '0 Bytes';
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB'];
+    const i = Math.floor(Math.log(value) / Math.log(k));
+    return parseFloat((value / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+  }
+}
