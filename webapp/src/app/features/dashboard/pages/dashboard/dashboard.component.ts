50361264339d3426f3e574413cbe2f199302917f
Davide Passafaro <passafaro@hotmail.it>
2026-02-02 00:33:18 +0100

    feat: refactor dashboard component with new file-card component

--- webapp/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ webapp/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { UploadFileComponent } from '../../components/upload-file/upload-file.component';
 import { FileService } from '../../../../core/services/file.service';
-import { FileListComponent } from '../../components/file-list/file-list.component';
-import { UploadedFile } from '../../../../shared/models/uploaded-file';
 import { UploadFileButtonComponent } from '../../components/upload-file-button/upload-file-button.component';
+import { FileCardComponent } from '../../components/file-card/file-card.component';
 
 @Component({
   selector: 'csv-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [UploadFileComponent, FileListComponent, UploadFileButtonComponent],
+  imports: [UploadFileComponent, FileCardComponent, UploadFileButtonComponent],
 })
 export class DashboardComponent {
   private fileService = inject(FileService);
