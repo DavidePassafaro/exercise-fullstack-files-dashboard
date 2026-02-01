ee5ae160ce39ac05153af26bae5e7ee3105e2771
Davide Passafaro <passafaro@hotmail.it>
2026-02-01 04:08:27 +0100

    feat: add click-outside directive

---
+++ webapp/src/app/shared/directives/click-outside.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostListener, inject, output } from '@angular/core';
+
+@Directive({ selector: '[csvClickOutside]' })
+export class ClickOutsideDirective {
+  elementRef = inject(ElementRef);
+
+  onOutsideClick = output();
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+    const element = this.elementRef.nativeElement;
+    if (!element.contains(clickedElement)) {
+      this.onOutsideClick.emit();
+    }
+  }
+}
