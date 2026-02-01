f5ac78993c850763c0ec8a40375a45569dbc5d34
Davide Passafaro <passafaro@hotmail.it>
2026-02-01 04:08:56 +0100

    feat: add user-dropdown component

---
+++ webapp/src/app/core/components/user-dropdown/user-dropdown.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, inject, input, signal } from '@angular/core';
+import { User } from '../../../shared/models/user';
+import { InitialsPipe } from '../../../shared/pipes/initials.pipe';
+import { AuthService } from '../../services/auth.service';
+import { ClickOutsideDirective } from '../../../shared/directives/click-outside.directive';
+
+@Component({
+  selector: 'csv-user-dropdown',
+  templateUrl: './user-dropdown.component.html',
+  styleUrls: ['./user-dropdown.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [InitialsPipe, ClickOutsideDirective],
+})
+export class UserDropdownComponent {
+  private authService = inject(AuthService);
+
+  user = input.required<User>();
+
+  isOpen = signal(false);
+
+  toggleDropdown() {
+    this.isOpen.set(!this.isOpen());
+  }
+
+  onLogoutClick() {
+    this.authService.logout().subscribe();
+  }
+}
