e3daa4f83575fd3897db94b555fcca28bbcc62a5
Davide Passafaro <passafaro@hotmail.it>
2026-02-01 02:22:40 +0100

    feat: add file-upload component in dashboard page

--- webapp/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ webapp/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { UploadFileComponent } from '../../components/upload-file/upload-file.component';
 
 @Component({
   selector: 'csv-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [UploadFileComponent],
 })
 export class DashboardComponent {}
