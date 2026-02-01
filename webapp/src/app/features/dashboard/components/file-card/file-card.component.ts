ecbb079a86c69ab9aa561962dcf05be3832314fa
Davide Passafaro <passafaro@hotmail.it>
2026-02-02 00:32:16 +0100

    feat: add file-card component

---
+++ webapp/src/app/features/dashboard/components/file-card/file-card.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { FileSizePipe } from '../../../../shared/pipes/file-size.pipe';
+
+@Component({
+  selector: 'csv-file-card',
+  templateUrl: './file-card.component.html',
+  styleUrls: ['./file-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FileSizePipe],
+})
+export class FileCardComponent {
+  name = input.required<string>();
+  size = input.required<number>();
+
+  removeFile = output<void>();
+
+  onRemoveFile() {
+    this.removeFile.emit();
+  }
+}
