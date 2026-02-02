6d527c645189d7dcb6fdf2fbe88d8c2929ee70d4
Davide Passafaro <passafaro@hotmail.it>
2026-02-02 14:20:56 +0100

    feat: add file-details component to file-preview page

--- webapp/src/app/features/dashboard/pages/file-preview/file-preview.component.ts
+++ webapp/src/app/features/dashboard/pages/file-preview/file-preview.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FileService } from '../../../../core/services/file.service';
+import { FileDetailsComponent } from '../../components/file-details/file-details.component';
 
 @Component({
   selector: 'csv-file-preview',
   templateUrl: './file-preview.component.html',
   styleUrls: ['./file-preview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FileDetailsComponent],
 })
-export class FilePreviewComponent {}
+export class FilePreviewComponent {
+  private route = inject(ActivatedRoute);
+  private fileService = inject(FileService);
+
+  file = computed(() =>
+    this.fileService
+      .filesList()
+      .find((file) => file._id === this.route.snapshot.paramMap.get('id')),
+  );
+}
