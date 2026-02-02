6a156478731be3f40c19b4dc64620b825d38f03e
Davide Passafaro <passafaro@hotmail.it>
2026-02-02 16:04:47 +0100

    feat: add data-type component

---
+++ webapp/src/app/features/dashboard/components/data-type-selector/data-type-selector.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+type DataType = 'text' | 'number' | 'date';
+
+@Component({
+  selector: 'csv-data-type-selector',
+  templateUrl: './data-type-selector.component.html',
+  styleUrls: ['./data-type-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FormsModule],
+})
+export class DataTypeSelectorComponent {
+  selectedType = model<DataType>('text');
+
+  protected readonly dataTypeLabels = new Map<DataType, string>([
+    ['text', 'Text'],
+    ['number', 'Number'],
+    ['date', 'Date'],
+  ]);
+
+  onTypeChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.selectedType.set(target.value as DataType);
+  }
+}
